37514d153cff2df6e53a8479e3ca0fb2597e6826
Andrew Volozhanin <linuxheadrus@gmail.com>
2015-06-29 00:40:57 +0500

    Cherry pick fix tabs

---
+++ spec/dummy/db/schema.rb
@@ -0,0 +1,29 @@
+ActiveRecord::Schema.define do
+
+  create_table :pages, force: true do |t|
+    t.string :slug
+    t.string :image
+    t.timestamps null: false
+  end
+
+  create_table :page_translations, force: true do |t|
+    t.integer  :page_id, null: false
+    t.string   :locale,     null: false
+    t.string   :title,      null: false
+    t.text   :content
+    t.timestamps null: false
+  end
+
+  create_table :admin_notes, force: true do |t|
+    t.string :image
+    t.timestamps null: false
+  end
+
+  create_table :admin_note_translations, force: true do |t|
+    t.integer  :note_id, null: false
+    t.string   :locale,     null: false
+    t.string   :title,      null: false
+    t.timestamps null: false
+  end
+
+end
